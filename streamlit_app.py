3e23367b0dfbe0e66a7f5161c339fb559dba9d4f
Kushagra Jaiswal <61771073+Nerd-coderZero@users.noreply.github.com>
2024-11-06 07:59:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -97,6 +97,43 @@ def process_video(uploaded_file, detection_model, pose, child_adult_model):
         # Read the output video for display
         with open(output_path, "rb") as f:
             return f.read()
+            
+def update_tracks(detections, tracks, iou_threshold=0.3, detection_threshold=0.7):
+    detection_boxes = detections['detection_boxes'].numpy()[0]
+    detection_scores = detections['detection_scores'].numpy()[0]
+    detection_classes = detections['detection_classes'].numpy()[0].astype(int)
+    
+    person_detections = [(i, box) for i, (box, score, cls) in enumerate(zip(detection_boxes, detection_scores, detection_classes)) 
+                         if cls == 1 and score > detection_threshold]
+    
+    updated_tracks = []
+    used_detections = set()
+    
+    for track_id, track_box, track_age in tracks:
+        best_iou = 0
+        best_detection = None
+        for detection_id, detection_box in person_detections:
+            if detection_id in used_detections:
+                continue
+            iou = compute_iou(track_box, detection_box)
+            if iou > best_iou:
+                best_iou = iou
+                best_detection = detection_id, detection_box
+        
+        if best_iou > iou_threshold:
+            updated_tracks.append((track_id, best_detection[1], track_age + 1))
+            used_detections.add(best_detection[0])
+        else:
+            if track_age > 0:
+                updated_tracks.append((track_id, track_box, track_age - 1))
+    
+    max_id = max([id for id, _, _ in tracks]) if tracks else 0
+    for detection_id, detection_box in person_detections:
+        if detection_id not in used_detections:
+            max_id += 1
+            updated_tracks.append((max_id, detection_box, 1))
+    
+    return updated_tracks
 
 def main():
     st.title("Person Detection and Classification")
