29ef53c8d559c7a05b784005c3031bdde96b167a
Kushagra Jaiswal <61771073+Nerd-coderZero@users.noreply.github.com>
2024-11-13 00:01:24 +0530

    Update object_detection.py

--- object_detection.py
+++ object_detection.py
@@ -8,6 +8,7 @@ from deep_sort_realtime.deepsort_tracker import DeepSort
 import logging
 from typing import Tuple, List, Dict, Optional
 import os
+import io
 
 class EnhancedPersonTracker:
     def __init__(
